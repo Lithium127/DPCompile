86f318cdf1f98609423f17679189c82a2090787e
Lithium127 <lman_gamer@wagzdomain.com>
2025-10-14 13:02:50 -0500

    Added Advancement Trigger Type

--- src/dpc/IO/advancement.py
+++ src/dpc/IO/advancement.py
@@ -4,11 +4,23 @@ import typing as t
 import json
 
 from .jsonfile import JsonFile
+from ..datatypes.textelement import to_textelement
 
+if t.TYPE_CHECKING:
+    from ..datatypes.textelement import TextElement
+
+class AdvancementTrigger():
+    pass
 
 class Advancement(JsonFile):
     """Represents an advancement that the player
     can achieve within the game."""
+
+    icon: dict[str, str]
+    frame: t.Literal["challenge", "goal", "task"]
+
+    title: TextElement
+    description: TextElement
     
     def __init__(self, name):
         super().__init__(name)
